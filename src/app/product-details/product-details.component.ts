e3c50d588be7a27ccedc3fabea1149e2c41e8fc7
mdlawrenceusa <mdlawrence@me.com>
2022-04-07 17:50:38 -0400

    1 file modified

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -9,6 +9,8 @@ import { Product, products } from '../products';
   styleUrls: ['./product-details.component.css']
 })
 export class ProductDetailsComponent implements OnInit {
+ 
+  product: Product | undefined;
 
   constructor(
 
